71579a957994736c3d6650d1bb01920d64d83e1b
waizmam <waizmam.rj@gmail.com>
2019-07-03 16:57:32 -0300

    Primeiro Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Adopt a new best friend</h1>
+    <button class="btn btn-primary">Add New Pet</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      showPetForm: false
+    }
+  },
+  methods: {
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    }
+  }
+}
+</script>
